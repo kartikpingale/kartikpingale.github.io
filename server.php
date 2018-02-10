0ff515d84997cb516bbd53a1f0523496958ada07
Kartik Pingale <kartikpgr8@gmail.com>
2018-02-10 20:06:39 +0530

    Create server.php

---
+++ server.php
@@ -0,0 +1,8 @@
+<?php
+  #connect to the database
+  $db = mysqli_connect('localhost', 'root', '', 'registration');
+  if(isset(var))
+  {
+    #code to connect the database and the form, reg, log in, msg;
+  }
+?>
